135d6ad1e20721770943311f248dbc90d105f316
kimkha <nkimkha@gmail.com>
2013-10-22 19:08:22 +0700

    Fix bug refresh icon.

--- background.js
+++ background.js
@@ -3,12 +3,13 @@ function refreshText() {
 	refreshTime();
 	var current = getCurrentLunarToday();
 	var text = "" + current.day;
-	var title = "";
+	var title = getDayName(current);
 	
 	chrome.browserAction.setIcon({path:"icon/"+current.day+".png"});
+	chrome.browserAction.setTitle({title:title});
 	//chrome.browserAction.setBadgeBackgroundColor({color:"#008"});
 	//chrome.browserAction.setBadgeText({text:text});
+	setTimeout(refreshText, 60*60*1000);
 }
 
 refreshText();
-setTimeout(refreshText, 60*60*1000);
